c31664e3c4b37ae6462dbd73c7451bb61a94fe20
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2022-12-30 11:11:50 +0530

    router done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='login' element={<Login />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   )
 }
 
 export default App
+//  <PrivateRoute>
+//               <Dashboard />
+//             </PrivateRoute>
